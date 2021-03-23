b5ad3023cd7b84c0f6059a9075e8f990bc60ad53
Jacob Hitze <jedmaster@gmail.com>
2021-03-22 23:31:37 -0400

    Now seeing data for 4g/5g connections

--- tmobile-logger.rb
+++ tmobile-logger.rb
@@ -11,8 +11,16 @@ key = settings["key"]
 username = settings["username"]
 
 # create an instance
-aio = Adafruit::IO::Client.new key: key, username: username
+#aio = Adafruit::IO::Client.new key: key, username: username
 
 tmobile_stats = Faraday.get 'http://192.168.12.1/fastmile_radio_status_web_app.cgi'
 
-puts tmobile_stats.body
+if (tmobile_stats.status != 200)
+  puts "T-Mobile device didn't respond as expected."
+  exit
+end
+
+tmobile_json = JSON.parse(tmobile_stats.body)
+puts "5g - #{tmobile_json["cell_5G_stats_cfg"]}"
+puts "4g - #{tmobile_json["cell_LTE_stats_cfg"]}"
+
