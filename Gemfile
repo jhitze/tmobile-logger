d604b40923333390380b9a742be4ffad58f01565
Jacob Hitze <jedmaster@gmail.com>
2021-03-22 23:15:40 -0400

    Now can pull data from T-Mobile box

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'adafruit-io'
+gem 'faraday'
 
--- tmobile-logger.rb
+++ tmobile-logger.rb
@@ -1,5 +1,5 @@
 require 'adafruit/io'
-require 'json'
+require 'faraday'
 
 #Read JSON from a file, iterate over objects
 file = open("settings.json")
@@ -12,3 +12,7 @@ username = settings["username"]
 
 # create an instance
 aio = Adafruit::IO::Client.new key: key, username: username
+
+tmobile_stats = Faraday.get 'http://192.168.12.1/fastmile_radio_status_web_app.cgi'
+
+puts tmobile_stats.body
